516462817a82928265bfe2e7032104c29ffd1035
passer-byzhang <1541115709@qq.com>
2024-04-14 00:16:59 +0800

    first commit

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -2,23 +2,37 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {BEX,IERC20DexModule} from "../src/BEXinterface.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+BEX constant bexpool = BEX(0x0d5862FDbdd12490f9b4De54c236cff63B038074);
 
+//forge test --rpc-url  https://artio.rpc.berachain.com -vvvvv
+
+contract BEXTest is Test {
+    address public USDC = 0x6581e59A1C8dA66eD0D313a0d4029DcE2F746Cc5;
+    address user = 0x0006C9919fbc3B2bb73f1f359499E982191bbc66;
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
     }
 
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+    function testSwap() public {
+                IERC20DexModule.BatchSwapStep memory step = IERC20DexModule.BatchSwapStep({
+            poolId:0x7D5b5C1937ff1b18B45AbC64aeAB68663a7a58Ab,
+            assetIn:0x0000000000000000000000000000000000000000,   // BERA
+            amountIn:20*1e15,  // 0.02
+            assetOut:USDC,
+            amountOut: 1000000000000000000,
+            userData:bytes("")
+            
+        });
+        vm.prank(user);
+        IERC20DexModule.BatchSwapStep[] memory steps = new IERC20DexModule.BatchSwapStep[](1);
+        steps[0] = step;
+        bexpool.batchSwap{value:20*1e15}(
+            0,
+            steps,
+            block.timestamp + 1000
+        );
     }
 
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
-    }
+    
 }
